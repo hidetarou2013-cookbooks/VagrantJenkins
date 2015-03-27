6d9d15e8003ec01642c786973ff94cfc791be501
maekawa@notepc2 <hidetoshi_maekawa@e-it.co.jp>
2015-03-27 14:52:42 +0900

    delete

--- site-cookbooks/mysettings/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default["mysettings"]["eth1_ip"] = "192.168.1.136"
-default["mysettings"]["eth2_ip"] = "192.168.33.136"
-default["mysettings"]["eth0_ip"] = "10.0.2.136"
-default["mysettings"]["hosts_ip"] = "192.168.1.136"
-default["mysettings"]["zabbix_server"] = "192.168.1.21"
-
--- site-cookbooks/mysettings/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'mysettings'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures mysettings'
-long_description 'Installs/Configures mysettings'
-version          '0.1.0'
-
