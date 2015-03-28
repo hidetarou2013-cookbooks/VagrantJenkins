a3cdb24baa88d60c857a7b831bcecec98fe5b59c
hmaekawa@notepc2 <hidetoshi_maekawa@e-it.co.jp>
2015-03-28 17:41:15 +0900

    edit shell

--- fluent_exec.sh
+++ fluent_exec.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 PWD=`pwd`
 echo $PWD
 #TARGETDIR=~/vagrant_dir/$1/
-TARGETDIR=~/vagrant_dir/VagrantJenkins-2/
+TARGETDIR=/home/maekawa/vagrant_dir/VagrantJenkins-2/
 cd $TARGETDIR
 echo $TARGETDIR
 
 touch run.lock
 
-git fetch origin master
-git pull origin master
+#git fetch origin master
+#git pull origin master
 #vagrant up
